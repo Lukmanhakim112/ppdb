c878376d765309c35e33422b6c1a2fac5dcc0989
Lukmanhakim112 <hlukman156@gmail.com>
2020-12-27 15:15:53 +0700

    create ajax for major form

--- primaseru/static/primaseru/js/primaseru.js
+++ primaseru/static/primaseru/js/primaseru.js
@@ -7,7 +7,6 @@ $(document).ready( () => {
     for (let x = 0; x < textbox.length; x++) {
         textbox[x].autoresize(1, 5);
     }
-
     $("#photo-profile-form").change(() => {
         document.forms["photo-profile-form"].submit();
     });
@@ -27,7 +26,6 @@ $(document).ready( () => {
 
     let formList = [
         [$("#profile-form"), "Calon Siswa", "profile"],
-        [$("#major-form"), "Jurusan", "major"],
         [$("#father-form"), "Ayah Calon Siswa", "father"],
         [$("#mother-form"), "Ibu Calon Siswa", "mother"],
         [$("#guardian-form"), "Wali Calon Siswa", "guardian"]
@@ -54,5 +52,56 @@ $(document).ready( () => {
         });
     }
 
+    let selectedFirst = $("#id_first_major").val();
+    if (!!(selectedFirst)) {
+        $("#id_first_major").prop('disabled', true);
+    }
+    let selectedSecond = $("#id_second_major").val();
+    if (!!(selectedSecond)) {
+        $("#id_second_major").prop('disabled', true);
+    }
+
+    let majorForm = $("#major-form");
+    $("#major-form").submit((e) => {
+        if ($("#id_first_major").is(":disabled") && $("#id_second_major").is(":disabled")) {
+            let alertDanger = `<div class="alert alert-danger alert-dismissible fade show" role="alert">Jurusan Sudah Dipilih, Tidak Bisa Diubah.... \
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">\
+                                    &times;</span></button></div>`;
+            $(majorForm).prepend(alertDanger);
+            e.preventDefault();
+            e.stopPropagation();
+        } else {
+            $.ajax({
+                url: "/profile/save/major/",
+                type: "POST",
+                data: $(majorForm).serialize(),
+                dataType: 'json',
+                beforeSend: () => {
+                    let alertDanger = `<div class="alert alert-danger alert-dismissible fade show" role="alert">Kedua juruan pilihan tidak boleh sama! \
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">\
+                                    &times;</span></button></div>`;
+                    if (selectedFirst == selectedSecond) {
+                        $(majorForm).prepend(alertDanger);
+                        return false;
+                    }
+                },
+                success: (response) => {
+                    $(majorForm).empty().html(response.form_s);
+                    console.log(response.form_s);
+                    let alertSuccess = `<div class="alert alert-success alert-dismissible fade show" role="alert">Berhasil Menyimpan Jurusan Pilihan \
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">\
+                                    &times;</span></button></div>`;
+                    $(majorForm).prepend(alertSuccess);
+                },
+                error: () => {
+                    let alertDanger = `<div class="alert alert-danger alert-dismissible fade show" role="alert">Data Tidak Valid, Silahkan Cek Kembali... \
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">\
+                                    &times;</span></button></div>`;
+                    $(majorForm).prepend(alertDanger);
+                },
+            });
+            return false;
+        }
+    });
 
 });
